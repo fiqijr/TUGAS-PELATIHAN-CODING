022907e34329e6935cc93f7e2fb0c26b03621dc3
fiqijr <113685352+fiqijr@users.noreply.github.com>
2022-09-16 21:13:01 +0700

    Add files via upload
    NAMA : RIFQI KHAIRAN FAATHIR
    NIM     : 220441100153
    RUANG : LABBIS

---
+++ Program Rumus Kubus dan Balok.py
@@ -0,0 +1,18 @@
+print("Program Menghitung Bangun Ruang Kubus")
+r = int(input("masukkan rusuk = "))
+Luas = 6 * r * r
+Volume = r * r * r
+print("Luas Kubus = ", Luas)
+print("Volume Kubus = ", Volume)
+
+
+print("Program Menghitung Bangun Ruang Balok")
+p = int(input("masukkan panjang = "))
+l = int(input("masukkan lebar = "))
+t = int(input("masukkan tingi = "))
+
+Luas = (2 * p * l)+(2 * p * l)+(2 * l * t)
+Volume = p * l * t
+
+print("Luas Balok = ", Luas)
+print("Volume Balok = ", Volume)
